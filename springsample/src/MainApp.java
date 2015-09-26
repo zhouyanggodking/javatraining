f91d5cf6bfda64d900cd911dc1085e22d78ab037
godking <zhou.yang.god.king@gmail.com>
2015-09-26 21:48:33 +0800

    spring sample

---
+++ springsample/src/MainApp.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import springsample.Person;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		Person p = (Person) context.getBean("godking");
+		System.out.println(p.getName());
+	}
+
+}
